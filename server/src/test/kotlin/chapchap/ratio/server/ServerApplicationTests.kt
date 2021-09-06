c4a932fc1ce810bd102627c41d5cee14c8bad5ce
yoonsung <yoonsung.park@navercorp.com>
2021-09-07 00:41:10 +0900

    first commit

---
+++ server/src/test/kotlin/chapchap/ratio/server/ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package chapchap.ratio.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
